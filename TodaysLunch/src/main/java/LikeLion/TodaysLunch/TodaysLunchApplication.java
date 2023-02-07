4d11b987ef3159d018131acc6fe804cd5daabe02
hdsung98 <hdsung98@gmail.com>
2023-02-08 01:18:08 +0900

    #2 feat: 기본 프로젝트 세팅

---
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/TodaysLunchApplication.java
@@ -0,0 +1,13 @@
+package LikeLion.TodaysLunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysLunchApplication.class, args);
+	}
+
+}
