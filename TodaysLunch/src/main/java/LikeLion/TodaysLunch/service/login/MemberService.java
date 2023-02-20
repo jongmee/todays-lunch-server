cb2dd9791d1ee1f349aba4f09257c2aa1e40faba
hdsung98 <hdsung98@naver.com>
2023-02-21 02:38:41 +0900

    #11 Fix: MemberService 재구현 - join: passwordEncoder 활용해 암호화한 상태로 db에 저장하도록 변경 - login: 예외처리 세분화, jwt 토큰을 반환하는 방식으로 변경

---
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/service/login/MemberService.java
@@ -0,0 +1,41 @@
+package LikeLion.TodaysLunch.service.login;
+
+import LikeLion.TodaysLunch.domain.Member;
+import LikeLion.TodaysLunch.dto.MemberDto;
+import LikeLion.TodaysLunch.repository.MemberRepository;
+import LikeLion.TodaysLunch.token.JwtTokenProvider;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(MemberDto memberDto) {
+        Member member = Member.builder()
+                .nickname(memberDto.getNickname())
+                .password(passwordEncoder.encode(memberDto.getPassword()))
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build();
+
+        return memberRepository.save(member).getId();
+    }
+
+    @Transactional
+    public String login(MemberDto memberDto) {
+        Member member = memberRepository.findByNickname(memberDto.getNickname())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+        if (!passwordEncoder.matches(memberDto.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+    }
+}
