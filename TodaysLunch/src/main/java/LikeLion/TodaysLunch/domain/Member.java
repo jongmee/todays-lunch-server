2b50c599243cc2717b665373e2b912b776503084
hdsung98 <hdsung98@naver.com>
2023-02-21 02:15:59 +0900

    #11 Fix: Member 도메인이 UserDetail implement 하도록 변경 - Role 부여(추후 접근범위 role에 의해 결정) - UserDetails의 주요 메서드 중 로그인에 필요한 것들 오버라이드

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/domain/Member.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/domain/Member.java
@@ -1,34 +1,52 @@
 package LikeLion.TodaysLunch.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class Member {
+@Builder
+public class Member implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nickname;
 
+    @Column(nullable = false)
     private String password;
 
     @ManyToOne
     private LocationCategory locationCategory;
     @ManyToOne
     private FoodCategory foodCategory;
+
+    @ElementCollection(fetch = FetchType.EAGER) //roles 컬렉션
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
 
     public Member(String nickname, String password) {
         this.nickname = nickname;
@@ -42,7 +60,8 @@ public class Member {
         return id;
     }
 
-    public String getNickname() {
+    @Override
+    public String getUsername() {
         return nickname;
     }
 
@@ -54,6 +73,31 @@ public class Member {
         return review;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
     public void updateLocationCategory(String locationCategory) {
         this.locationCategory = new LocationCategory(locationCategory);
     }
