cee8bc9276bc9bb90d5c2e5a6acde30f129d1b33
hdsung98 <hdsung98@naver.com>
2023-02-23 18:02:23 +0900

    #21 Add: TokenDto 생성 - 향후 토큰 전달은 이 DTO 바탕으로 함

---
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/dto/TokenDto.java
@@ -0,0 +1,27 @@
+package LikeLion.TodaysLunch.dto;
+
+
+import LikeLion.TodaysLunch.token.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import java.util.Date;
+
+public class TokenDto {
+
+    private final String token;
+    private final Date expirationTime;
+
+    public TokenDto(String token) {
+        this.token = token;
+        this.expirationTime = JwtTokenProvider.getExpirationTime(token);
+    }
+
+    public Object getToken() {
+        return this.token;
+    }
+
+    public Date getTokenExpiresTime() {
+        return this.expirationTime;
+    }
+}
