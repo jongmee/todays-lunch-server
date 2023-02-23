80fa08fd25a664952264c29963f4bcecef342780
hdsung98 <hdsung98@naver.com>
2023-02-23 18:11:33 +0900

    #21 MemberService login 로직 변경 - 로그인 시 발급받은 토큰 Redis DB에 serialize하여 저장 - 이후 로그아웃에 저장한 토큰 활용

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/service/login/MemberService.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/service/login/MemberService.java
@@ -2,10 +2,12 @@ package LikeLion.TodaysLunch.service.login;
 
 import LikeLion.TodaysLunch.domain.Member;
 import LikeLion.TodaysLunch.dto.MemberDto;
+import LikeLion.TodaysLunch.dto.TokenDto;
 import LikeLion.TodaysLunch.repository.MemberRepository;
 import LikeLion.TodaysLunch.token.JwtTokenProvider;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,7 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
     private final MemberRepository memberRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
     public Long join(MemberDto memberDto) {
@@ -30,19 +33,23 @@ public class MemberService {
     }
 
     private void validateDuplication(MemberDto memberDto) {
-        if(memberRepository.findByNickname(memberDto.getNickname()).isPresent()){
+        if (memberRepository.findByNickname(memberDto.getNickname()).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
         }
     }
 
     @Transactional
-    public String login(MemberDto memberDto) {
+    public TokenDto login(MemberDto memberDto) {
         Member member = memberRepository.findByNickname(memberDto.getNickname())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
         if (!passwordEncoder.matches(memberDto.getPassword(), member.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
-        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+        TokenDto tokenDto = jwtTokenProvider.createToken(member.getNickname(), member.getRoles());
+        redisTemplate.opsForValue()
+                .set(member.getNickname(), tokenDto.getToken());
+
+        return tokenDto;
     }
 }
