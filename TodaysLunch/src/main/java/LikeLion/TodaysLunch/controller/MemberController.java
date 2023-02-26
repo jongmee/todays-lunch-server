36338efbe50e1ca8836dabf6a8990a4765e1e9bb
hdsung98 <hdsung98@naver.com>
2023-02-27 00:02:27 +0900

    #21 Fix: Logout 컨트롤러 ResponseEntity 형식으로 리턴값 변경 - 에러 메시지, 성공 메시지도 body에 담아 전달하도록 구성

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/controller/MemberController.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/controller/MemberController.java
@@ -5,9 +5,12 @@ import LikeLion.TodaysLunch.dto.TokenDto;
 import LikeLion.TodaysLunch.service.login.MemberService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,8 +33,13 @@ public class MemberController {
     }
 
     @PutMapping("/logout-member")
-    public void logout(@RequestBody TokenDto tokenDto) {
-        memberService.logout(tokenDto);
+    public ResponseEntity<String> logout(@RequestHeader("token") TokenDto tokenDto) {
+        try{
+            memberService.logout(tokenDto);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+        return ResponseEntity.ok("성공적으로 로그아웃했습니다.");
     }
 
 }
