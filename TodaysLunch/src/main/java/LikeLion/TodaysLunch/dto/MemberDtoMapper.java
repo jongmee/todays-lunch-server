632c722677164a621bcabed0a470c16d71902414
hdsung98 <hdsung98@naver.com>
2023-02-28 00:21:45 +0900

    #24 Add: 멤버 도메인의 대부분의 필드 가져올 수 있도록 멤버dto에 필드 업데이트, MemberDtoMapper 생성 - MemberDtoMapper Detail: 인가된 멤버의 정보를 dto에 담아 리턴하는 메서드. 향후 memberservice의 mypage 등의 API에서 활용할 예정

---
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/dto/MemberDtoMapper.java
@@ -0,0 +1,18 @@
+package LikeLion.TodaysLunch.dto;
+
+import LikeLion.TodaysLunch.domain.Member;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDtoMapper {
+
+    public static MemberDto toDto(Member member) {
+        MemberDto dto = new MemberDto();
+        dto.setNickname(member.getNickname());
+        dto.setPassword(member.getPassword());
+        dto.setLocationCategory(member.getLocationCategory());
+        dto.setFoodCategory(member.getFoodCategory());
+        dto.setImageUrl(member.getImageUrl());
+        return dto;
+    }
+}
