645ab287ea7f38223f0e4b373aabf27e37072088
jongmee <ra123221@gmail.com>
2023-03-24 02:41:31 +0900

    #16 Add: 메뉴 Entity 수정, Dto 생성
    - 메뉴 Entity에 해당 메뉴의 사진 개수를 저장하는 필드를 추가하였다.
    - 메뉴를 생성하고 조회하는 기능에서 쓰일 메뉴 Dto를 생성하였다.

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/domain/Menu.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/domain/Menu.java
@@ -9,12 +9,15 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 public class Menu {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,11 +25,16 @@ public class Menu {
   @Column(nullable = false)
   private String name;
   private Long price;
+  private Long imageCount;
   @ManyToOne
   @JoinColumn
   private Restaurant restaurant;
   @OneToOne
   @JoinColumn
   private Sale sale;
-
+  @Builder
+  public Menu(String name, Long price){
+    this.name = name;
+    this.price = price;
+  }
 }
