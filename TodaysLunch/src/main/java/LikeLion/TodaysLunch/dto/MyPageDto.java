9ee2c8078d821206257f33944b50d40bb47fe5cf
jongmee <ra123221@gmail.com>
2023-05-14 00:52:34 +0900

    #32 Add: 마이페이지의 조회 기능 API에서 사용할 DTO 생성

---
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/dto/MyPageDto.java
@@ -0,0 +1,53 @@
+package LikeLion.TodaysLunch.dto;
+
+import LikeLion.TodaysLunch.domain.Member;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MyPageDto {
+  private Long id;
+  private String nickname;
+  private String icon;
+  private Boolean verified;
+  private List<FoodCategoryDto> foodCategoryList;
+  private List<LocationCategoryDto> locationCategoryList;
+  private Integer myJudgeCount;
+  private Integer  participationCount;
+  private Integer contributionCount;
+  private Integer myStoreCount;
+  private Integer reviewCount;
+  public static MyPageDto fromEntity(Member member,
+      List<FoodCategoryDto> foodCategoryList,
+      List<LocationCategoryDto> locationCategoryList,
+      Integer myJudgeCount,
+      Integer participationCount,
+      Integer myStoreCount,
+      Integer reviewCount,
+      Integer contributionCount){
+    String image = null;
+    if (member.getIcon() != null){
+      image = member.getIcon().getImageUrl();
+    }
+    return MyPageDto.builder()
+        .id(member.getId())
+        .nickname(member.getNickname())
+        .icon(image)
+        .verified(member.getVerified())
+        .foodCategoryList(foodCategoryList)
+        .locationCategoryList(locationCategoryList)
+        .myJudgeCount(myJudgeCount)
+        .participationCount(participationCount)
+        .myStoreCount(myStoreCount)
+        .reviewCount(reviewCount)
+        .contributionCount(contributionCount)
+        .build();
+  }
+
+}
