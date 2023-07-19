756bd6acfbdf3db564d09944b141e12833f406d6
jongmee <ra123221@gmail.com>
2023-07-19 20:24:09 +0900

    #40 Update: category service와 그에 대한 test에서 사용할 레포지토리 메서드 추가

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/repository/RestRecmdRelRepository.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/repository/RestRecmdRelRepository.java
@@ -1,8 +1,10 @@
 package LikeLion.TodaysLunch.repository;
 
+import LikeLion.TodaysLunch.domain.Restaurant;
 import LikeLion.TodaysLunch.domain.relation.RestaurantRecommendCategoryRelation;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestRecmdRelRepository extends JpaRepository<RestaurantRecommendCategoryRelation,Long> {
-
+  List<RestaurantRecommendCategoryRelation> findAllByRestaurant(Restaurant restaurant);
 }
