50ce27b61c7663fa44608c66f76a0d37b227eaaa
jongmee <ra123221@gmail.com>
2023-07-27 16:29:50 +0900

    #49 Test: 리뷰를 등록하는 기능 테스트하기
    - 리뷰를 등록하고 각 리뷰의 평점의 평균이 맛집의 평점에 반영되는지 확인한다.

--- TodaysLunch/src/test/java/LikeLion/TodaysLunch/review/service/ReviewServiceTest.java
+++ TodaysLunch/src/test/java/LikeLion/TodaysLunch/review/service/ReviewServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import LikeLion.TodaysLunch.member.domain.Member;
 import LikeLion.TodaysLunch.restaurant.domain.Restaurant;
 import LikeLion.TodaysLunch.review.domain.Review;
+import LikeLion.TodaysLunch.review.dto.ReviewDto;
 import LikeLion.TodaysLunch.review.repository.ReviewRepository;
 import LikeLion.TodaysLunch.skeleton.ServiceTest;
 import LikeLion.TodaysLunch.skeleton.TestRestaurant;
@@ -39,6 +40,47 @@ class ReviewServiceTest extends ServiceTest {
     //then
     Restaurant restaurantForTest = testRestaurantEnviron.restaurantRepository().findById(1L).get();
     Assertions.assertEquals("아주맛잇군2", restaurantForTest.getBestReview());
+  }
+  @Test
+  void 리뷰_등록하기(){
+    // given
+    TestUser 유저1 = makeTestUser("qwer@naver.com", "1234", "유저1", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
+
+    TestRestaurant 맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "가츠벤또","정말 맛있다", 126.940155, 37.546924, 유저1.getMember());
+    ReviewDto reviewDto1 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(1).build();
+    ReviewDto reviewDto2 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(2).build();
+
+    // when
+    reviewService.create(1L, reviewDto1, 유저1.getMember());
+    reviewService.create(1L, reviewDto2, 유저1.getMember());
+
+    //then
+    Assertions.assertEquals(2L, 맛집.getRestaurant().getReviewCount());
+  }
+  @Test
+  void 리뷰_등록하고_맛집_평점에_반영하기(){
+    // given
+    TestUser 유저1 = makeTestUser("qwer@naver.com", "1234", "유저1", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
+
+    TestRestaurant 맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "가츠벤또","정말 맛있다", 126.940155, 37.546924, 유저1.getMember());
+    ReviewDto reviewDto1 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(1).build();
+    ReviewDto reviewDto2 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(2).build();
+    ReviewDto reviewDto3 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(5).build();
+    ReviewDto reviewDto4 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(3).build();
+    ReviewDto reviewDto5 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(3).build();
+
+    // when
+    reviewService.create(1L, reviewDto1, 유저1.getMember());
+    Double rating1 = 맛집.getRestaurant().getRating();
+    reviewService.create(1L, reviewDto2, 유저1.getMember());
+    reviewService.create(1L, reviewDto3, 유저1.getMember());
+    reviewService.create(1L, reviewDto4, 유저1.getMember());
+    reviewService.create(1L, reviewDto5, 유저1.getMember());
+    Double rating2 = 맛집.getRestaurant().getRating();
+
+    //then
+    Assertions.assertEquals(1.0, rating1);
+    Assertions.assertEquals((1.0+2.0+5.0+3.0+3.0)/5, rating2);
   }
 
   Review 리뷰_생성하기(Member member, Restaurant restaurant, String reviewContent, Integer rating){
