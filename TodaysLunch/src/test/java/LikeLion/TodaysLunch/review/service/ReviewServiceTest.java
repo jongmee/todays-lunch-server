f814de8684396a495ebca442ec27f5c8a78ff6cb
jongmee <ra123221@gmail.com>
2023-07-27 16:37:54 +0900

    #49 Test: 리뷰를 수정하고 해당 리뷰가 맛집 평점에 반영되는지 테스트하기

--- TodaysLunch/src/test/java/LikeLion/TodaysLunch/review/service/ReviewServiceTest.java
+++ TodaysLunch/src/test/java/LikeLion/TodaysLunch/review/service/ReviewServiceTest.java
@@ -82,6 +82,28 @@ class ReviewServiceTest extends ServiceTest {
     Assertions.assertEquals(1.0, rating1);
     Assertions.assertEquals((1.0+2.0+5.0+3.0+3.0)/5, rating2);
   }
+  @Test
+  void 리뷰를_수정하고_맛집_평점에_반영하기(){
+    // given
+    TestUser 유저1 = makeTestUser("qwer@naver.com", "1234", "유저1", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
+
+    TestRestaurant 맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "가츠벤또","정말 맛있다", 126.940155, 37.546924, 유저1.getMember());
+    ReviewDto 작성한_리뷰 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(5).build();
+    ReviewDto 작성한_리뷰2 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(2).build();
+    ReviewDto 작성한_리뷰3 = ReviewDto.builder().reviewContent("정말 맛있는 집이예요!").rating(5).build();
+
+    reviewService.create(1L, 작성한_리뷰, 유저1.getMember());
+    reviewService.create(1L, 작성한_리뷰2, 유저1.getMember());
+    reviewService.create(1L, 작성한_리뷰3, 유저1.getMember());
+    Double 기존평점 = 맛집.getRestaurant().getRating();
+
+    // when
+    ReviewDto 수정한_리뷰 = ReviewDto.builder().reviewContent("생각해보니까 별로인듯").rating(1).build();
+    reviewService.update(1L, 1L, 수정한_리뷰);
+    Double 수정된평점 = 맛집.getRestaurant().getRating();
+    Assertions.assertEquals((5.0+2.0+5.0)/3, 기존평점);
+    Assertions.assertEquals((1.0+2.0+5.0)/3, 수정된평점);
+  }
 
   Review 리뷰_생성하기(Member member, Restaurant restaurant, String reviewContent, Integer rating){
     Review review = new Review(reviewContent, rating);
