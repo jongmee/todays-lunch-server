e38a0f4e4fc85f54461d734d0bdd6d10301323d2
jongmee <ra123221@gmail.com>
2023-08-06 14:49:38 +0900

    #52 Test: 유저가 작성한 리뷰의 목록을 조회하는 API를 컨트롤러 단에서 테스트하기
    - 테스트를 통과하였다

--- TodaysLunch/src/test/java/LikeLion/TodaysLunch/review/controller/ReviewControllerTest.java
+++ TodaysLunch/src/test/java/LikeLion/TodaysLunch/review/controller/ReviewControllerTest.java
@@ -1,7 +1,23 @@
 package LikeLion.TodaysLunch.review.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import LikeLion.TodaysLunch.review.dto.MyReviewDto;
 import LikeLion.TodaysLunch.skeleton.ControllerTest;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 
 public class ReviewControllerTest extends ControllerTest {
@@ -10,4 +26,31 @@ public class ReviewControllerTest extends ControllerTest {
   void 리뷰_등록하기() throws Exception {
 
   }
+  @Test
+  @WithMockUser(username="test", roles={"SUPER"})
+  void 유저가_작성한_리뷰목록_반환하기() throws Exception {
+    // given
+    HashMap<String, Object> 응답 = new HashMap<>();
+    MyReviewDto 리뷰 = MyReviewDto.builder()
+        .reviewId(1L)
+        .restaurantId(1L)
+        .restaurantName("test name")
+        .imageUrl("https://dummy-image-url")
+        .reviewContent("dummy review content")
+        .rating(1)
+        .createdDate(LocalDate.of(2023, 2, 3))
+        .likeCount(1L)
+        .liked("false").build();
+    응답.put("data", new ArrayList<>(Arrays.asList(리뷰)));
+    응답.put("totalPages", 1);
+    BDDMockito.given(reviewService.myReviewList(anyLong(), anyInt(), anyInt(), any(), any())).willReturn(응답);
+    // when & then
+    mockMvc.perform(get("/myreviews")
+            .param("reviewer-id", "1")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+
 }
