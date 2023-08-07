6f3beb16b051e27c193dccf9fefc8eae8d1ec596
jongmee <ra123221@gmail.com>
2023-08-07 15:23:01 +0900

    #57 Test: 메뉴 DTO에서 validation 검사하는 것을 테스트하기

---
+++ TodaysLunch/src/test/java/LikeLion/TodaysLunch/menu/controller/MenuControllerTest.java
@@ -0,0 +1,42 @@
+package LikeLion.TodaysLunch.menu.controller;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import LikeLion.TodaysLunch.menu.dto.MenuDto;
+import LikeLion.TodaysLunch.skeleton.ControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+class MenuControllerTest extends ControllerTest {
+  @Test
+  @WithMockUser(username="test", roles={"SUPER"})
+  void 메뉴_올바르게_등록하기() throws Exception {
+    MenuDto 메뉴_생성_요청 = MenuDto.builder().name("메뉴 이름").price(15000L).build();
+
+    // when & then
+    mockMvc.perform(post("/restaurants/{restaurantId}/menus", 1L)
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(메뉴_생성_요청)))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+  @Test
+  @WithMockUser(username="test", roles={"SUPER"})
+  void 메뉴_잘못된_형식으로_등록하기() throws Exception {
+    // given
+    MenuDto 메뉴_생성_요청 = MenuDto.builder().price(100L).build();
+
+    // when & then
+    mockMvc.perform(post("/restaurants/{restaurantId}/menus", 1L)
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(메뉴_생성_요청)))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+  }
+}
