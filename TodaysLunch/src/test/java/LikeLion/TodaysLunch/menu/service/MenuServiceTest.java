5ea5baae175e9b7e3d15c1395084c56b34280af9
jongmee <ra123221@gmail.com>
2023-08-16 21:40:51 +0900

    #70 Test, Refactor: 메뉴를 등록, 수정, 삭제했을 때 해당 맛집의 최저 메뉴 가격에 올바르게 반영되는지 테스트하기

--- TodaysLunch/src/test/java/LikeLion/TodaysLunch/menu/service/MenuServiceTest.java
+++ TodaysLunch/src/test/java/LikeLion/TodaysLunch/menu/service/MenuServiceTest.java
@@ -2,17 +2,20 @@ package LikeLion.TodaysLunch.menu.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import LikeLion.TodaysLunch.exception.NotFoundException;
 import LikeLion.TodaysLunch.image.domain.ImageUrl;
 import LikeLion.TodaysLunch.image.domain.MenuImage;
 import LikeLion.TodaysLunch.image.repository.ImageUrlRepository;
 import LikeLion.TodaysLunch.image.repository.MenuImageRepository;
+import LikeLion.TodaysLunch.member.domain.Member;
 import LikeLion.TodaysLunch.menu.domain.Menu;
 import LikeLion.TodaysLunch.menu.dto.MenuDto;
 import LikeLion.TodaysLunch.menu.dto.MenuImageDto;
 import LikeLion.TodaysLunch.menu.dto.SaleMenuDto;
 import LikeLion.TodaysLunch.menu.repository.MenuRepository;
+import LikeLion.TodaysLunch.restaurant.domain.Restaurant;
 import LikeLion.TodaysLunch.skeleton.service.ServiceTest;
 import LikeLion.TodaysLunch.skeleton.service.TestRestaurant;
 import LikeLion.TodaysLunch.skeleton.service.TestUser;
@@ -82,15 +85,8 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴1 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴1, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴1 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
-
-    MenuDto 새로운_메뉴2 = MenuDto.builder().name("명란크림우동").price(10000L).salePrice(9000L).build();
-    menuService.create(새로운_메뉴2, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴2 = menuRepository.findByName("명란크림우동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴1 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
+    Menu 등록된_메뉴2 = 메뉴_생성하기("명란크림우동", 10000L, 9000L, null, 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     // when
     MenuDto 수정_요청1 = MenuDto.builder().name("사케동").price(15000L).build();
@@ -110,10 +106,7 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     String 이미지_이름 = "test_logo.jpg";
     MultipartFile 이미지 = 이미지_가져오기(이미지_이름);
@@ -137,10 +130,7 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     String 이미지_이름 = "test_logo.jpg";
     MultipartFile 이미지 = 이미지_가져오기(이미지_이름);
@@ -161,10 +151,7 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     String 이미지_이름 = "test_logo.jpg";
     MultipartFile 이미지 = 이미지_가져오기(이미지_이름);
@@ -189,10 +176,7 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     String 이미지_이름 = "test_logo.jpg";
     MultipartFile 이미지 = 이미지_가져오기(이미지_이름);
@@ -230,10 +214,7 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     MenuDto 수정_요청 = MenuDto.builder().name("사케동").price(15000L).build();
     menuService.update(수정_요청, 정식맛집.getRestaurant().getId(), 등록된_메뉴.getId(), 유저.getMember());
@@ -241,7 +222,7 @@ class MenuServiceTest extends ServiceTest {
     LocalDateTime 비교시간 = LocalDateTime.now();
 
     // when
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
+    메뉴_생성하기("가츠동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
     LocalDateTime 수정시간 = 정식맛집.getRestaurant().getUpdatedDate();
 
     //then
@@ -254,10 +235,7 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     String 이미지_이름 = "test_logo.jpg";
     MultipartFile 이미지 = 이미지_가져오기(이미지_이름);
@@ -278,10 +256,7 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    Menu 등록된_메뉴 = menuRepository.findByName("사케동")
-        .orElseThrow(() -> new NotFoundException("메뉴"));
+    Menu 등록된_메뉴 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     // when
     MenuDto 수정_요청 = MenuDto.builder().name("사케동").price(15000L).build();
@@ -299,12 +274,9 @@ class MenuServiceTest extends ServiceTest {
     TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
     TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
 
-    MenuDto 새로운_메뉴1 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).saleExplain("서강대학생전용입니다").build();
-    menuService.create(새로운_메뉴1, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    MenuDto 새로운_메뉴2 = MenuDto.builder().name("사케동").price(15000L).salePrice(10000L).build();
-    menuService.create(새로운_메뉴2, 정식맛집.getRestaurant().getId(), 유저.getMember());
-    MenuDto 새로운_메뉴3 = MenuDto.builder().name("사케동").price(15000L).build();
-    menuService.create(새로운_메뉴3, 정식맛집.getRestaurant().getId(), 유저.getMember());
+    메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
+    메뉴_생성하기("가츠동", 15000L, 10000L, null, 정식맛집.getRestaurant().getId(), 유저.getMember());
+    메뉴_생성하기("에비동", 15000L, null, null, 정식맛집.getRestaurant().getId(), 유저.getMember());
 
     // when
     Pageable pageable = PageRequest.of(0, 5);
@@ -314,6 +286,52 @@ class MenuServiceTest extends ServiceTest {
     List<SaleMenuDto> 세일메뉴목록 = (List<SaleMenuDto>) 응답.get("data");
     assertEquals(2, 세일메뉴목록.size());
   }
+
+  @Test
+  void 메뉴_등록시_맛집의_최저메뉴에_반영하기(){
+    // given
+    TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
+    TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
+
+    // when
+    Menu 등록된_메뉴1 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
+    Menu 등록된_메뉴2 = 메뉴_생성하기("가츠동", 9000L, 7000L, null, 정식맛집.getRestaurant().getId(), 유저.getMember());
+    Menu 등록된_메뉴3 = 메뉴_생성하기("에비동", 6000L, null, null, 정식맛집.getRestaurant().getId(), 유저.getMember());
+
+    // then
+    assertEquals(6000L, 정식맛집.getRestaurant().getLowestPrice());
+  }
+
+  @Test
+  void 메뉴_삭제시_맛집의_최저메뉴에_반영하기(){
+    // given
+    TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
+    TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
+
+    // when
+    Menu 등록된_메뉴1 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
+    Menu 등록된_메뉴2 = 메뉴_생성하기("가츠동", 9000L, 7000L, null, 정식맛집.getRestaurant().getId(), 유저.getMember());
+    menuService.delete(정식맛집.getRestaurant().getId(), 등록된_메뉴2.getId());
+
+    // then
+    assertEquals(10000L, 정식맛집.getRestaurant().getLowestPrice());
+  }
+
+  @Test
+  void 메뉴_수정시_맛집의_최저메뉴에_반영하기(){
+    // given
+    TestUser 유저 = makeTestUser("qwer@naver.com", "1234", "유저", new ArrayList<>(Arrays.asList("한식")), new ArrayList<>(Arrays.asList("서강대")));
+    TestRestaurant 정식맛집 = makeTestRestaurant("한식", "서강대", "정문", "서울시 마포구", "정든그릇", "정말 맛있는 집!", 37.546924, 126.940155, 유저.getMember());
+
+    // when
+    Menu 등록된_메뉴1 = 메뉴_생성하기("사케동", 15000L, 10000L, "서강대학생전용입니다", 정식맛집.getRestaurant().getId(), 유저.getMember());
+    MenuDto 수정_요청 = MenuDto.builder().name("사케동").price(15000L).build();
+    menuService.update(수정_요청, 정식맛집.getRestaurant().getId(), 등록된_메뉴1.getId(), 유저.getMember());
+
+    // then
+    assertEquals(15000L, 정식맛집.getRestaurant().getLowestPrice());
+  }
+
 
   private MultipartFile 이미지_가져오기(String imageName) throws IOException {
     File file = new File(new File("").getAbsolutePath() + "/src/test/resources/testImage/"+imageName);
@@ -325,5 +343,12 @@ class MenuServiceTest extends ServiceTest {
 
     MultipartFile mFile = new CommonsMultipartFile(fileItem);
     return mFile;
+  }
+
+  Menu 메뉴_생성하기(String 메뉴이름, Long 가격, Long 세일가격, String 세일정보, Long 맛집ID, Member 등록자){
+    MenuDto 새로운_메뉴 = MenuDto.builder().name(메뉴이름).price(가격).salePrice(세일가격).saleExplain(세일정보).build();
+    menuService.create(새로운_메뉴, 맛집ID, 등록자);
+    return menuRepository.findByName(메뉴이름)
+        .orElseThrow(() -> new NotFoundException("메뉴"));
   }
 }
