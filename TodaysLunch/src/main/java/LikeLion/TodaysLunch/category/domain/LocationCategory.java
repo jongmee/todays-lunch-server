5b39e3e9e48138f6aacd644762bdd3fff068dd2d
jongmee <ra123221@gmail.com>
2023-11-23 16:08:19 +0900

    #86 Refactor(LocationTag, LocationCategory): 위치 카테고리 및 태그와의 거기를 구하는 메서드를 엔티티 클래스 내부에 구현하기
    - ISSUE: 값과 로직을 한 곳에서 관리하고자 서비스 코드에서 빼서 엔티티 클래스 내부에 정의하였지만, 위치 카테고리와 위치 태그의 필드가 모두 일치하여 중복되는 메서드가 생성되었다. 두 엔티티 클래스를 어떻게 관리할 것인지 추후 고민해야 한다.:

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/category/domain/LocationCategory.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/category/domain/LocationCategory.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class LocationCategory {
+  private static final double EARTH_RADIUS = 6370d;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +35,13 @@ public class LocationCategory {
     return this;
   }
 
+  public double getDistance(double latitude, double longitude) {
+    double latitudeDifference = Math.toRadians(latitude - this.latitude);
+    double longitudeDifference = Math.toRadians(longitude - this.longitude);
+    double raw = Math.cos(Math.toRadians(latitude))
+            * Math.cos(Math.toRadians(this.latitude))
+            * Math.pow(Math.sin(longitudeDifference),2)
+            + Math.pow(Math.sin(latitudeDifference/2), 2);
+    return EARTH_RADIUS * 2 * Math.atan2(Math.sqrt(raw), Math.sqrt(1-raw));
+  }
 }
